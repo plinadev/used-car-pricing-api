4bf93f0f32fc21ce94e6f62b1c6a96c96fe79c91
Polina Pavlenko <pavlenkopolina415@gmail.com>
2025-06-30 19:11:59 +0300

    feat: Add e2e tests for authentication

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/signup (POST)', () => {
+    const email = 'e2etest1@mail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: '1234567',
+      })
+      .expect(HttpStatus.CREATED)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
